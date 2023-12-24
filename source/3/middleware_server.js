e72dddcf65f2e7ab1d815aef69490f20e4cc54f5
iacobucci <iacobuccivalerio@gmail.com>
2023-12-24 19:43:05 +0100

    modified:   slides/1.md 	modified:   slides/2.md 	modified:   slides/3.md 	renamed:    source/3/static_server.js -> source/3/express_server.js 	renamed:    source/3/hello_server.js -> source/3/http_server.js 	new file:   source/3/middleware_server.js

---
+++ source/3/middleware_server.js
@@ -0,0 +1,12 @@
+import express from 'express'; // importiamo il modulo express
+
+const app = express(); // creiamo l'applicazione
+
+app.use(express.static('public')); // serviamo i files statici
+
+app.listen(3000); // mettiamo in ascolto l'applicazione sulla porta 3000
+
+function middleware1(req, res, next) {
+	console.log(req.body);
+	next();
+}
